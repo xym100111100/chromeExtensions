7e0d2d889392701e12f55d416e97cdc83b3e5364
umbrella <253274870@qq.com>
2020-09-14 10:28:29 +0800

    添加自己的哈士奇

---
+++ hashiqiTest/hashiqi/js/hashiqi.js
@@ -0,0 +1,12 @@
+import Vue  from "./vue"
+
+
+ new Vue({
+    el: '.tm-fcs-panel',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+
+//document.getElementsByClassName('tm-fcs-panel')[0].style.background="red"
